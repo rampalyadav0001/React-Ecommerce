23f042453848dcc5583d09f55219ff4f3e933cde
rampal <rampalyadav23082001@gmail.com>
2023-10-11 17:20:12 +0530

    checkPoint 9: loginAuth added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import Home from './pages/Home';
-import Cart from './features/cart/cart';
-import SignUpPage from './pages/SignUpPage';
-import LoginPage from './pages/LoginPage';
 import { Counter } from './features/counter/Counter';
-import ProductList from './features/product-list/components/ProductList';
-import ProductDetailsPage from './pages/ProductDetailPage';
 import './App.css';
+import Home from './pages/Home';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignUpPage';
 
 import {
   createBrowserRouter,
@@ -15,13 +10,23 @@ import {
   Route,
   Link,
 } from 'react-router-dom';
+import Cart from './features/cart/cart';
 import CartPage from './pages/CartPage';
 import Checkout from './pages/Checkout';
-
+import ProductDetailPage from './pages/ProductDetailPage';
+import Protected from './features/auth/component/Protected';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectLoggedInUser } from './features/auth/authSlice';
+import { fetchItemsByUserIdAsync } from './features/cart/cartSlice';
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Home></Home>,
+    element: (
+      <Protected>
+        <Home></Home>
+      </Protected>
+    ),
   },
   {
     path: '/login',
@@ -29,24 +34,47 @@ const router = createBrowserRouter([
   },
   {
     path: '/signup',
-    element: <SignUpPage></SignUpPage>,
+    element: <SignupPage></SignupPage>,
   },
   {
     path: '/cart',
-    element: <CartPage></CartPage>,
+    element: (
+      <Protected>
+        <CartPage></CartPage>
+      </Protected>
+    ),
   },
   {
     path: '/checkout',
-    element: <Checkout></Checkout>,
+    element: (
+      <Protected>
+        <Checkout></Checkout>
+      </Protected>
+    ),
   },
   {
     path: '/product-detail/:id',
-    element: <ProductDetailsPage></ProductDetailsPage>,
+    element: (
+      <Protected>
+        <ProductDetailPage></ProductDetailPage>
+      </Protected>
+    ),
   },
 ]);
+
 function App() {
+
+  const dispatch = useDispatch();
+  const user = useSelector(selectLoggedInUser);
+
+  useEffect(()=>{
+    if(user){
+      dispatch(fetchItemsByUserIdAsync(user.id))
+    }
+  },[dispatch, user])
+
   return (
-    <div className='App'>
+    <div className="App">
       <RouterProvider router={router} />
     </div>
   );
